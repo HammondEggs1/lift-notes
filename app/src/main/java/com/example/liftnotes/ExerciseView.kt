06e3d766c3357b0ef0a1427994f9bc905bf11ac1
HammondEggs1 <zhammond10643@yahoo.com>
2024-09-26 11:08:31 -0400

    Made basic layout

---
+++ app/src/main/java/com/example/liftnotes/ExerciseView.kt
@@ -0,0 +1,40 @@
+package com.example.liftnotes;
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.liftnotes.databinding.ExerciseViewBinding
+
+class ExerciseView : Fragment() {
+    private var _binding: ExerciseViewBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View {
+
+        _binding = ExerciseViewBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.back.setOnClickListener {
+            findNavController().navigate(R.id.action_ExerciseView_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/example/liftnotes/FirstFragment.kt
+++ app/src/main/java/com/example/liftnotes/FirstFragment.kt
@@ -14,26 +14,22 @@ import com.example.liftnotes.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.buttonOne.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        binding.exerciseButton.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_ExerciseView)
         }
     }
 
