d92b30869e4e1bb39b9267ea12cd6399c7fbb6a7
t0rtureddrag0n <t0rtureddrag0n@outlook.com>
2024-10-28 22:30:21 -0400

    switch to graphview, canned vico, and androidplot

--- app/src/main/java/com/example/liftnotes/ExerciseView.kt
+++ app/src/main/java/com/example/liftnotes/ExerciseView.kt
@@ -2,39 +2,29 @@ package com.example.liftnotes;
 
 import ExHistoryAdapter
 import android.content.BroadcastReceiver
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.runtime.Composable
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.androidplot.xy.CatmullRomInterpolator
-import com.androidplot.xy.LineAndPointFormatter
-import com.androidplot.xy.PanZoom
-import com.androidplot.xy.SimpleXYSeries
-import com.androidplot.xy.XYGraphWidget
-import com.androidplot.xy.XYPlot
-import com.androidplot.xy.XYSeries
 import com.example.liftnotes.databinding.ExerciseViewBinding
-import java.text.FieldPosition
-import java.text.Format
-import java.text.ParsePosition
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
 
 
 class ExerciseView : Fragment() {
     private var _binding: ExerciseViewBinding? = null
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: ExHistoryAdapter
-    private lateinit var plot: XYPlot
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
     private lateinit var receiver: BroadcastReceiver
     private lateinit var dayName: String
-
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
@@ -53,44 +43,20 @@ class ExerciseView : Fragment() {
         val data = listOf("10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs", "10/8/24 | 3x12 | 200 lbs")
         adapter = ExHistoryAdapter(data)
         recyclerView.adapter = adapter
-
+        val graph = binding.graph
+        val series = LineGraphSeries(
+            arrayOf(
+                DataPoint(0.0, 1.0),
+                DataPoint(1.0, 5.0),
+                DataPoint(2.0, 3.0),
+                DataPoint(3.0, 2.0),
+                DataPoint(4.0, 6.0)
+            )
+        )
+        graph.addSeries(series)
         binding.back.setOnClickListener {
             findNavController().navigate(R.id.action_ExerciseView_to_FirstFragment)
         }
-        val domainLabels = arrayOf<Number>(1,2,3,6,7,8,9,10,13,14)
-        val series1Number = arrayOf<Number>(1,4,8,12,16,32,26,29,10,13)
-        val series2Number = arrayOf<Number>(2,8,4,7,32,16,64,12,7,10)
-
-        val series1 : XYSeries = SimpleXYSeries(
-            listOf(* series1Number),SimpleXYSeries.ArrayFormat.Y_VALS_ONLY
-            ,"Series 1")
-        val series2 : XYSeries = SimpleXYSeries(
-            listOf(* series2Number),SimpleXYSeries.ArrayFormat.Y_VALS_ONLY
-            ,"Series 1")
-
-        val series1Format = LineAndPointFormatter(null, null, null, null)
-        val series2Format = LineAndPointFormatter(null,null,null,null)
-
-        series1Format.interpolationParams = CatmullRomInterpolator.Params(10,
-            CatmullRomInterpolator.Type.Centripetal)
-        series2Format.interpolationParams = CatmullRomInterpolator.Params(10,
-            CatmullRomInterpolator.Type.Centripetal)
-        //plot.addSeries(series1,series1Format)//this causes crash
-        //plot.addSeries(series2,series2Format)
-        //plot.graph.getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = object : Format() {
-            //override fun format(
-                //obj: Any?,
-               // toAppendTo: StringBuffer,
-               // pos: FieldPosition
-            //): StringBuffer {
-        //        val i = Math.round((obj as Number).toFloat())
-        //        return toAppendTo.append(domainLabels[i])
-         //   }
-        //    override fun parseObject(source: String?, pos: ParsePosition): Any? {
-      //          return null
-     //       }
-
-     //   }
     }
 
     override fun onDestroyView() {
